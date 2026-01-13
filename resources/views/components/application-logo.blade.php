a8a20749b359d9aca57e612ac0add01795c346be
Matt Nicolaysen <matt@nicolaysen.net>
2026-01-12 21:28:43 -0700

    Update logo to line-art seedling style

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,10 @@
-<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <!-- Sprouting seed/leaf icon -->
-    <path fill="currentColor" d="M12 22c-1.1 0-2-.9-2-2v-5.5c-1.7-.8-3-2.4-3-4.3 0-1.5.7-2.9 1.8-3.8C8.3 4.7 7 3 7 3s3.5.5 5 2.5C13.5 3.5 17 3 17 3s-1.3 1.7-1.8 3.4c1.1.9 1.8 2.3 1.8 3.8 0 1.9-1.3 3.5-3 4.3V20c0 1.1-.9 2-2 2z"/>
-    <path fill="currentColor" d="M12 2C9.5 2 7 4 7 4s2 1 3.5 1c.5 0 1-.1 1.5-.3.5.2 1 .3 1.5.3 1.5 0 3.5-1 3.5-1s-2.5-2-5-2z" opacity="0.6"/>
+<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" {{ $attributes }}>
+    <!-- Stem -->
+    <path d="M12 20V10"/>
+    <!-- Left leaf -->
+    <path d="M12 14C12 14 8 13 7 9C7 9 10 8 12 10"/>
+    <!-- Right leaf -->
+    <path d="M12 10C12 10 16 9 17 5C17 5 14 4 12 6"/>
+    <!-- Seed at bottom -->
+    <path d="M12 20L9 17C8 19 9 21 12 22C15 21 16 19 15 17L12 20Z"/>
 </svg>
